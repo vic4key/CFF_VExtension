1b54eb1a1b5b7730baaebb3470a11f6d1822b5d1
Vic P <vic4key@gmail.com>
2022-05-09 02:23:49 +0700

    Initial

---
+++ VExtension.Page.cpp
@@ -0,0 +1,97 @@
+#include <vu>
+#include <fstream>
+#include <windowsx.h>
+
+#include "VExtension.Page.h"
+#include "types.h"
+#include "resource.h"
+
+extern std::string get_prefs_file_path();
+
+VExtensionPage::VExtensionPage(json& prefs)
+  : m_prefs(prefs)
+  , m_option_resolve_ordinal(nullptr)
+  , m_option_undecorate_symbol(nullptr)
+  , m_option_shorten_undecorated_symbol(nullptr)
+{
+}
+
+VExtensionPage::~VExtensionPage()
+{
+}
+
+LRESULT VExtensionPage::DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  switch (uMsg)
+  {
+  case WM_INITDIALOG:
+    {
+      m_option_resolve_ordinal = GetDlgItem(hWnd, IDC_OPTION_RESOLVE_ORDINAL);
+      m_option_undecorate_symbol = GetDlgItem(hWnd, IDC_OPTION_UNDECORATE_SYMBOL);
+      m_option_shorten_undecorated_symbol = GetDlgItem(hWnd, IDC_OPTION_SHORTEN_UNDECORATED_SYMBOL);
+
+      bool resolve_ordinal = json_get_option(m_prefs, "resolve_ordinal", true);
+      Button_SetCheck(m_option_resolve_ordinal, resolve_ordinal ? BST_CHECKED : BST_UNCHECKED);
+
+      bool undecorate_symbol = json_get_option(m_prefs, "undecorate_symbol", true);
+      Button_SetCheck(m_option_undecorate_symbol, undecorate_symbol ? BST_CHECKED : BST_UNCHECKED);
+
+      bool shorten_undecorated_symbol = json_get_option(m_prefs, "shorten_undecorated_symbol", true);
+      Button_SetCheck(m_option_shorten_undecorated_symbol, shorten_undecorated_symbol ? BST_CHECKED : BST_UNCHECKED);
+      Button_Enable(m_option_shorten_undecorated_symbol, undecorate_symbol ? TRUE : FALSE);
+    }
+    break;
+
+  case WM_COMMAND:
+    {
+      switch (LOWORD(wParam))
+      {
+        case IDC_OPTION_RESOLVE_ORDINAL:
+        {
+          bool resolve_ordinal = Button_GetCheck(m_option_resolve_ordinal) == BST_CHECKED;
+          json_set_option(m_prefs, "resolve_ordinal", resolve_ordinal);
+        }
+        break;
+
+        case IDC_OPTION_UNDECORATE_SYMBOL:
+        {
+          bool undecorate_symbol = Button_GetCheck(m_option_undecorate_symbol) == BST_CHECKED;
+          json_set_option(m_prefs, "undecorate_symbol", undecorate_symbol);
+          Button_Enable(m_option_shorten_undecorated_symbol, undecorate_symbol ? TRUE : FALSE);
+        }
+        break;
+
+        case IDC_OPTION_SHORTEN_UNDECORATED_SYMBOL:
+        {
+          bool shorten_undecorated_symbol = Button_GetCheck(m_option_shorten_undecorated_symbol) == BST_CHECKED;
+          json_set_option(m_prefs, "shorten_undecorated_symbol", shorten_undecorated_symbol);
+        }
+        break;
+
+        case IDC_OPTION_SAVE:
+        {
+          try
+          {
+            auto prefs_file_path = get_prefs_file_path();
+            std::ofstream f;
+            f.open(prefs_file_path);
+            f << m_prefs.dump(2, ' ');
+            f.close();
+          }
+          catch (...)
+          {
+            auto error = vu::get_last_error_A();
+            vu::msg_box_A(hWnd, MB_OK | MB_ICONERROR, "VExtension", "Save failed (%s).", error.c_str());
+          }
+        }
+        break;
+      }
+    }
+    break;
+
+  default:
+    break;
+  }
+
+  return FALSE;
+}
---
+++ VExtension.Page.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <windows.h>
+#include "types.h"
+
+class VExtensionPage
+{
+public:
+  VExtensionPage(json& prefs);
+  virtual ~VExtensionPage();
+
+  virtual LRESULT DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+  json& m_prefs;
+  HWND  m_option_resolve_ordinal;
+  HWND  m_option_undecorate_symbol;
+  HWND  m_option_shorten_undecorated_symbol;
+};
